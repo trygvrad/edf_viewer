7e302a5ccd9dc3b30c298739f9060c59fae35158
trygvrad <trygvrad@gmail.com>
2022-02-03 13:12:38 +0100

    cleanup

--- RunPyinstaller.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\trygvrad\AppData\Local\Programs\Python\Python39\Scripts\pyinstaller.exe --windowed --icon=iconOri.ico loader.py
-# --console --onefile
-%Pause
--- edf_view.bat
+++
@@ -1,4 +0,0 @@
-C:\Users\trygvrad\AppData\Local\Programs\Python\Python39\Scripts\pip3.9.exe install pyinstaller pyqt5 pyqtgraph fabio
-C:\Users\trygvrad\AppData\Local\Programs\Python\Python39\Python.exe loader.py
-
-%Pause
